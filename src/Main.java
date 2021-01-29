37ed84e224d23c56db6481233af00e194bff2517
Vivian Ng <vivian.ng.t.y@gmail.com>
2021-01-28 20:00:28 -0500

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+// Write a Java program that reverse a word.
+// Write a Java program that prints all prime numbers from 1-20 using a while loop.
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(reverseWord("Vivian"));
+        primeNumbers();
+        getUp(7);
+        // String myString = "perscholas";
+        // String random = new StringBuilder(myString).reverse().toString();
+        // System.out.println(random);
+    }
+    public static String reverseWord(String word) {
+        int i, length = word.length();
+        StringBuilder newWord = new StringBuilder(length);
+        for (i = (length - 1); i >= 0; i--){
+            newWord.append(word.charAt(i));
+        }
+        return newWord.toString();
+    }
+
+    static void primeNumbers() {
+        int primeCheckNumber;
+        int n = 1;
+        int divisibleCount;
+
+        while (n <= 20) {
+            divisibleCount = 0;
+            primeCheckNumber = 2;
+            while (primeCheckNumber <= n / 2) {
+                if (n % primeCheckNumber == 0) {
+                    divisibleCount++;
+                    break;
+                }
+                primeCheckNumber++;
+            }
+            if (divisibleCount == 0 && n != 1) {
+                System.out.print(n + " ");
+            }
+            n++;
+        }
 
     }
+
+    static void getUp (boolean squawking, int currentHour){
+        if ( currentHour < 6 || currentHour > 22 ){
+            System.out.println("Get up");
+        } else {
+            System.out.println("keep sleeping");
+        }
+    }
+
 }
+
+
